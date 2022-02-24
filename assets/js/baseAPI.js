af6116dcd99cee75e03292d20b9c52083d16b8b5
emy <2080156129@qq.com>
2022-02-25 04:00:09 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 $.ajaxPrefilter(function(options){
     // 在发起ajax请求之前拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    
+    // 为有权限的接口设置 headers 请求头
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局挂载 complete 回调函数
+    options.complete = function (res) {
+        // 如果用户未登录跳转至登录页
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    getUserInfo()
+
+    // 点击退出按钮实现
+    $('#btnLogout').on('click', function () {
+        // 是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            // 移除token 并跳转页面
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+
+            // 关闭询问框
+            layer.close(index);
+        });
+    })
+})
+
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染用户信息头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+    } else {
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
